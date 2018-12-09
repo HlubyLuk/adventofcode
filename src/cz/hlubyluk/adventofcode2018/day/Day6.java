ab87a34dadfb439427c2dfeb50881821acf63c4d
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2018-12-09 10:58:39 +0100

    Day 6, just template.

---
+++ src/cz/hlubyluk/adventofcode2018/day/Day6.java
@@ -0,0 +1,57 @@
+package cz.hlubyluk.adventofcode2018.day;
+
+import java.util.Arrays;
+
+/**
+ * https://adventofcode.com/2018/day/6
+ *
+ * @author HlubyLuk
+ */
+public class Day6 implements IDay6 {
+  private static final int MATRIX_EDGE = 10;
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see cz.hlubyluk.adventofcode2018.day.IDay#solveFirst()
+   */
+  @Override
+  public String solveFirst() {
+//    char[][] matrix = new char[Day6.MATRIX_EDGE][Day6.MATRIX_EDGE];
+//    for (int y = 0; y < matrix.length; y += 1) {
+//      char[] row = matrix[y];
+//
+//      for (int x = 0; x < row.length; x += 1) {
+//        row[x] = '.';
+//      }
+//    }
+//
+//    char tmp = 'A';
+//
+//    for (String line : IDay6.TEST_INPUT.split("\n")) {
+//      String[] split = line.split(", ");
+//      int x = Integer.valueOf(split[0]);
+//      int y = Integer.valueOf(split[1]);
+//
+//      matrix[y][x] = tmp;
+//
+//      tmp += 1;
+//    }
+//
+//    Arrays.stream(matrix).forEach(x -> System.out.println(Arrays.toString(x)));
+//
+    return null;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see cz.hlubyluk.adventofcode2018.day.IDay#solveSecond()
+   */
+  @Override
+  public String solveSecond() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
