534b1b06a647b46e71f49ffdc3607144d6492f7e
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2018-12-23 19:22:16 +0100

    Day 14, part 1.

---
+++ src/cz/hlubyluk/adventofcode2018/day/Day14.java
@@ -0,0 +1,56 @@
+package cz.hlubyluk.adventofcode2018.day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day14 implements IDay14 {
+
+  @Override
+  public String solveFirst() {
+    int first = 0, second = 1;
+    List<Character> scores = new ArrayList<>();
+    scores.add('3');
+    scores.add('7');
+
+    Integer stop = Integer.valueOf(String.valueOf(IDay14.INPUT));
+    for (int i = 0; i < stop + 10; i += 1) {
+      Character a = scores.get(first);
+      Character b = scores.get(second);
+      char[] add = this.add(a, b);
+
+      for (char c : add) {
+        scores.add(c);
+      }
+
+      first = this.index(scores, first);
+      second = this.index(scores, second);
+    }
+
+    StringBuilder builder = new StringBuilder();
+    for (int i = stop; i < stop + 10; i += 1) {
+      builder.append(scores.get(i));
+    }
+
+    return builder.toString();
+  }
+
+  private char[] add(char a, char b) {
+    int c = (a - '0') + (b - '0');
+
+    return String.valueOf(c).toCharArray();
+  }
+
+  private int index(List<Character> scores, int idx) {
+    return (idx + Integer.valueOf(String.valueOf(scores.get(idx))) + 1) % scores.size();
+  }
+
+  @Override
+  public String solveSecond() {
+    return null;
+  }
+
+  @Override
+  public String getTag() {
+    return "2018 Day14";
+  }
+}
---
+++ src/cz/hlubyluk/adventofcode2018/day/IDay14.java
@@ -0,0 +1,7 @@
+package cz.hlubyluk.adventofcode2018.day;
+
+import cz.hlubyluk.adventofcode2018.IDay;
+
+public interface IDay14 extends IDay {
+  char[] INPUT = "190221".toCharArray();
+}
