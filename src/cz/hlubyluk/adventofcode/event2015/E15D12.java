9145e7bcb53e30a6546d64e0628555565e78cb48
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2018-12-31 12:50:32 +0100

    Event 2015, day 12, part 1.

--- src/cz/hlubyluk/adventofcode/event2015/E15D12.java
+++ src/cz/hlubyluk/adventofcode/event2015/E15D12.java
@@ -3,6 +3,10 @@
  */
 package cz.hlubyluk.adventofcode.event2015;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.Scanner;
+
 import cz.hlubyluk.adventofcode.event2015.input.IE15D12;
 
 /**
@@ -29,8 +33,32 @@ public class E15D12 implements IE15D12 {
    */
   @Override
   public String solveFirst() {
-    // TODO Auto-generated method stub
-    return null;
+    StringBuilder builder = new StringBuilder();
+
+    boolean space = false;
+    StringCharacterIterator it = new StringCharacterIterator(IE15D12.INPUT);
+    for (char c = it.current(); c != CharacterIterator.DONE; c = it.next()) {
+      if (Character.isDigit(c) || c == '-') {
+        space = true;
+        builder.append(c);
+      } else if (space) {
+        space = false;
+        builder.append(' ');
+      }
+    }
+
+    int count = 0;
+    Scanner sc = new Scanner(builder.toString());
+    while (sc.hasNext()) {
+      count += sc.nextInt();
+    }
+    sc.close();
+
+    if (count != 156366) {
+      throw new RuntimeException("Wrong!!!");
+    }
+
+    return String.valueOf(count);
   }
 
   /*
