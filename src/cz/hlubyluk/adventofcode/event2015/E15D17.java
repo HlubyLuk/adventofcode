220969ef8a0121fc5b8e67b8ef9a8de72752d2cd
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2019-01-07 22:25:56 +0100

    Event 2015, day 17.

--- src/cz/hlubyluk/adventofcode/event2015/E15D17.java
+++ src/cz/hlubyluk/adventofcode/event2015/E15D17.java
@@ -3,6 +3,11 @@
  */
 package cz.hlubyluk.adventofcode.event2015;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 import cz.hlubyluk.adventofcode.event2015.input.IE15D17;
 
 /**
@@ -11,6 +16,79 @@ import cz.hlubyluk.adventofcode.event2015.input.IE15D17;
  * @author HlubyLuk
  */
 public class E15D17 implements IE15D17 {
+  private static final Solver SOLVER = new Solver();
+
+  private static class Solver {
+    private static final Parser PARSER = new Parser();
+    static {
+      Solver.PARSER.parse();
+    }
+
+    private Solver() {
+    }
+
+    private int part1() {
+      Integer counter = 0;
+
+      int pow = (int) Math.pow(2, Solver.PARSER.containers.size());
+
+      for (int i = 1; i < pow; i += 1) {
+        int count = 0;
+
+        String code = Integer.toBinaryString(pow | i);
+        for (int j = 1; j < code.length(); j += 1) {
+          if (code.charAt(j) == '1') {
+            count += Solver.PARSER.containers.get(j - 1);
+          }
+        }
+
+        if (count == IE15D17.LITERS) {
+          counter += 1;
+        }
+      }
+
+      return counter;
+    }
+
+    private int part2() {
+      TreeMap<Integer, Integer> cache = new TreeMap<>();
+
+      int pow = (int) Math.pow(2, Solver.PARSER.containers.size());
+
+      for (int i = 1; i < pow; i += 1) {
+        int liters = 0, count = 0;
+
+        String code = Integer.toBinaryString(pow + i);
+        for (int j = 1; j < code.length(); j += 1) {
+          if (code.charAt(j) == '1') {
+            liters += Solver.PARSER.containers.get(j - 1);
+            count += 1;
+          }
+        }
+
+        if (IE15D17.LITERS == liters) {
+          cache.put(count, cache.getOrDefault(count, 0) + 1);
+        }
+      }
+
+      return cache.firstEntry().getValue();
+    }
+  }
+
+  private static class Parser {
+    private final List<Integer> containers = new ArrayList<>();
+
+    private Parser() {
+    }
+
+    private void parse() {
+      Scanner sc = new Scanner(IE15D17.INPUT);
+      while (sc.hasNext()) {
+        this.containers.add(sc.nextInt());
+      }
+      sc.close();
+    }
+  }
 
   /*
    * (non-Javadoc)
@@ -29,8 +107,7 @@ public class E15D17 implements IE15D17 {
    */
   @Override
   public String solveFirst() {
-    // TODO Auto-generated method stub
-    return null;
+    return this.result(1304, E15D17.SOLVER.part1());
   }
 
   /*
@@ -40,7 +117,6 @@ public class E15D17 implements IE15D17 {
    */
   @Override
   public String solveSecond() {
-    // TODO Auto-generated method stub
-    return null;
+    return this.result(18, E15D17.SOLVER.part2());
   }
 }
