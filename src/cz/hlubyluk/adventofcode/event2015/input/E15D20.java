a67638552f1b3af3817324e61f8b8616305f2c58
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2019-01-13 21:32:01 +0100

    Event 2015, day 20.

---
+++ src/cz/hlubyluk/adventofcode/event2015/input/E15D20.java
@@ -0,0 +1,67 @@
+/**
+ *
+ */
+package cz.hlubyluk.adventofcode.event2015.input;
+
+/**
+ * https://adventofcode.com/2015/day/20
+ *
+ * @author HlubyLuk
+ *
+ */
+public class E15D20 implements IE15D20 {
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see cz.hlubyluk.adventofcode.IDay#getTag()
+   */
+  @Override
+  public String getTag() {
+    return "2015 day 20";
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see cz.hlubyluk.adventofcode.IDay#solveFirst()
+   */
+  @Override
+  public String solveFirst() {
+    final int max = IE15D20.INPUT / 10;
+    final int[] houses = new int[max];
+    int last = Integer.MAX_VALUE;
+
+    for (int i = 1; i < max; i += 1) {
+      for (int j = i; j < max; j += i) {
+        if ((houses[j] += i) >= max && j < last) {
+          last = j;
+        }
+      }
+    }
+
+    return this.result(786240, last);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see cz.hlubyluk.adventofcode.IDay#solveSecond()
+   */
+  @Override
+  public String solveSecond() {
+    final int max = IE15D20.INPUT / 10;
+    final int[] houses = new int[max];
+    int last = Integer.MAX_VALUE;
+
+    for (int i = 1; i < max; i += 1) {
+      for (int j = i, k = 0; j < max && k < 50; j += i, k += 1) {
+        if ((houses[j] += i * 11) >= IE15D20.INPUT && j < last) {
+          last = j;
+        }
+      }
+    }
+
+    return this.result(831600, last);
+  }
+}
